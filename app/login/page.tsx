fa9c5f48fb2dd9afc7e18caec698b3dbb2bb65e3
rusira <rusirajayatilake96@gmail.com>
2025-03-03 14:28:08 +1300

    initial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+// import { LoginFormOne } from "@/src/components/login-form-one";
+import { LoginFormTwo } from "@/src/components/login/login-form-two";
+
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        {/* <LoginFormOne /> */}
+        <LoginFormTwo />
+      </div>
+    </div>
+  );
+}
